f43912c7295a33fd200149ebb928d9e6f5574519
Gerald Goh <88106820+Wizualizer@users.noreply.github.com>
2026-03-15 02:31:58 +0000

    message

--- test/pipeline/pipeline.py
+++ test/pipeline/pipeline.py
@@ -4,8 +4,9 @@ import pandas as pd
 month = int(sys.argv[1])
 
 df = pd.DataFrame({"A": [1,2], "B": [3,4]})
-print(df)
+
 df['month'] = month
+print(df)
 df.to_parquet(f"output_{month}.parquet")
 
 print('arguments', sys.argv)
