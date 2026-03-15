3932e5dfb30417cb859bc1d5c1683dd47edc6ba9
Gerald Goh <88106820+Wizualizer@users.noreply.github.com>
2026-03-15 16:48:16 +0000

    added

--- pipeline/Commands.sh
+++ pipeline/Commands.sh
@@ -6,15 +6,39 @@ docker run -it --rm \
   -p 5432:5432 \
   postgres:18
 
-
+  #postgres container
   docker run -it \
   -e POSTGRES_USER="root" \
   -e POSTGRES_PASSWORD="root" \
   -e POSTGRES_DB="ny_taxi" \
   -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql \
   -p 5432:5432 \
+  --network=pg-network \
+  --name pgdatabase \
   postgres:18
 
   uv run pgcli -h localhost -p 5432 -u root -d ny_taxi
 
   uv run jupyter notebook
+
+
+  uv run python ingest_data.py \
+  --pg-user=root \
+  --pg-pass=root \
+  --pg-host=localhost \
+  --pg-port=5432 \
+  --pg-db=ny_taxi \
+  --target-table=yellow_taxi_trips_2021_1 \
+  --chunksize=100000
+
+  #dockerfile containing ingestion script
+  docker run -it --rm \
+  --network=pg-network \
+  taxi_ingest:v001 \
+    --pg-user=root \
+    --pg-pass=root \
+    --pg-host=pgdatabase \
+    --pg-port=5432 \
+    --pg-db=ny_taxi \
+    --target-table=yellow_taxi_trips_2021_1 \
+    --chunksize=100000
